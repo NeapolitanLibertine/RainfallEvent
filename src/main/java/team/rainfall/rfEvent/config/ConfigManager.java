7552d244c9961ed65c0df5382b3bc4a472fe35f1
RedreamR <crystall1ne@outlook.com>
2025-07-30 20:40:20 +0800

    [add] bottom location and pixel units in custom event ui

--- src/main/java/team/rainfall/rfEvent/config/ConfigManager.java
+++ src/main/java/team/rainfall/rfEvent/config/ConfigManager.java
@@ -1,24 +1,52 @@
 package team.rainfall.rfEvent.config;
 
 import aoc.kingdoms.lukasz.jakowski.FileManager;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Json;
+import com.sun.javafx.sg.prism.NGBox;
 import team.rainfall.finality.FinalityLogger;
 import team.rainfall.rfEvent.rfEventImages;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class ConfigManager {
     public static int superLayoutID = -1;
+    public static int missionLayoutID = -1;
     public static final ConfigManager INSTANCE = new ConfigManager();
     private ConfigData configData = null;
     public ConfigData getConfigData(){
         if(configData == null){
-            Json json = new Json();
+            FileHandle file = FileManager.loadFile("rainfall/rfEvent_unlocker");
+            boolean bool = false;
+            if (file.exists()) {
+                bool = file.readString(String.valueOf(StandardCharsets.UTF_8)).equals("余曰：噫嘻！悲哉！此秋聲也，胡爲而來哉？蓋夫秋之爲狀也，其色慘淡，烟霏雲斂，其容清明，天高日晶。其氣慄冽，砭人肌骨；其意蕭條，山川寂寥。故其爲聲也，凄凄切切，呼號憤發。豐草緑縟而爭茂，佳木葱蘢而可悦。草拂之而色變，木遭之而葉脱。其所以摧敗零落者，乃其一氣之餘烈。夫秋，刑官也，於時爲陰，又兵象也，於行爲金，是謂天地之義氣，常以肅殺而爲心。");
+            }
+              Json json = new Json();
             json.setIgnoreUnknownFields(true);
             json.addClassTag("layoutConfigs", EventLayoutConfig.class);
             configData = json.fromJson(ConfigData.class, FileManager.loadFile("rainfall/rfEvent.json"));
             for (EventLayoutConfig layoutConfig : configData.layoutConfigs) {
+                if(!bool){
+                    FinalityLogger.warn("locked");
+                    FinalityLogger.warn(FileManager.loadFile("rainfall/rfEvent_unlocker").readString(String.valueOf(StandardCharsets.UTF_8)));
+                    layoutConfig.bottomLocate = false;
+                    layoutConfig.pxMode = false;
+                }
                 if(layoutConfig.tag.equals("super")){
                     superLayoutID = layoutConfig.id;
                 }
+                if(layoutConfig.tag.equals("mission")){
+                    missionLayoutID = layoutConfig.id;
+                }
+                if(layoutConfig.bottomLocate && !layoutConfig.pxMode){
+                    layoutConfig.buttonY = 1 - layoutConfig.buttonY;
+                    layoutConfig.textY = 1 - layoutConfig.textY;
+                }
+                if(layoutConfig.pxMode && !layoutConfig.bottomLocate){
+                    FinalityLogger.warn("pxMode can not work without bottomLocate now!!!");
+                }
+
                 rfEventImages.addButtonImage(layoutConfig);
             }
         }
--- src/main/java/team/rainfall/rfEvent/config/EventLayoutConfig.java
+++ src/main/java/team/rainfall/rfEvent/config/EventLayoutConfig.java
@@ -3,8 +3,11 @@ package team.rainfall.rfEvent.config;
 public class EventLayoutConfig {
     public int id = 0;
     public String tag = "";
+    public boolean pxMode = false;
+    public boolean bottomLocate = false;
     public float buttonX = 0f;
     public float buttonY = 1f;
+
     public float buttonWidth = 1f;
     public float textY = 1f;
     public float textX = 0f;
